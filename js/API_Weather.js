ef479534479a8c67424de4a7dc4daf5f00ed5c78
charlotte <brasseurcharlotte28@gmail.com>
2021-12-06 16:55:14 +0100

    ok

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -4,6 +4,8 @@
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL1 = "https://api.openweathermap.org/data/2.5/forecast/daily";
+
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
@@ -25,6 +27,29 @@ class API_WEATHER{
       crossdomain: true
     })
   }
+  
+  fetchD1Forecast(){
+    return axios
+    .get(`${API_URL1}?q=${this.city}&units=metric&cnt=1&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+  }
+
+  fetchD2Forecast(){
+    return axios
+    .get(`${API_URL1}?q=${this.city}&units=metric&cnt=2&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+  }
+
+  fetchD3Forecast(){
+    return axios
+    .get(`${API_URL1}?q=${this.city}&units=metric&cnt=3&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+  }
+
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 
 // Fonction appelée lors du click du bouton
 function start() {
-  // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  // Création de l'objet apiWeather;
+    var city = document.getElementById("city-input").value;
+  console.log(city);
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
@@ -16,6 +18,8 @@ function start() {
       const description = data.weather[0].description;
       const temp = data.main.temp;
       const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      var city = apiWeather.city;
+ 
 
       // Modifier le DOM
       document.getElementById('today-forecast-main').innerHTML = main;
@@ -24,8 +28,67 @@ function start() {
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
       
     })
+
+    .fetchD1Forecast()
+    .then(function(response) {
+      const data = response.data;
+
+      // On récupère l'information principal
+      const main = data.weather[1].main;
+      const description = data.weather[1].description;
+      const temp = data.main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.weather[1].icon);
+  
+
+      // Modifier le DOM
+      document.getElementById('D1-forecast-main').innerHTML = main;
+      document.getElementById('D1-forecast-more-info').innerHTML = description;
+      document.getElementById('icon-weather-container').innerHTML = icon;
+      document.getElementById('D1-forecast-temp').innerHTML = `${temp}°C`;
+      
+    })
+
+    .fetchD2Forecast()
+    .then(function(response) {
+      const data = response.data;
+
+      // On récupère l'information principal
+      const main = data.weather[2].main;
+      const description = data.weather[2].description;
+      const temp = data.main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.weather[2].icon);
+  
+
+      // Modifier le DOM
+      document.getElementById('D2-forecast-main').innerHTML = main;
+      document.getElementById('D2-forecast-more-info').innerHTML = description;
+      document.getElementById('icon-weather-container').innerHTML = icon;
+      document.getElementById('D2-forecast-temp').innerHTML = `${temp}°C`;
+      
+    })
+
+    .fetchD3Forecast()
+    .then(function(response) {
+      console.log(response.data);
+
+      // On récupère l'information principal
+      const main = data.weather[3].main;
+      const description = data.weather[3].description;
+      const temp = data.main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.weather[3].icon);
+  
+
+      // Modifier le DOM
+      document.getElementById('D3-forecast-main').innerHTML = main;
+      document.getElementById('D3-forecast-more-info').innerHTML = description;
+      document.getElementById('icon-weather-container').innerHTML = icon;
+      document.getElementById('D3-forecast-temp').innerHTML = `${temp}°C`;
+      
+    })
+
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
     });
+    
 }
